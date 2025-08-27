06c9d87fb0553ca56607afd8504d541bd1676ded
lamThanakit <h58284836@gmail.com>
2025-08-27 15:34:49 +0700

    img fix

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
             {/* รูปภาพด้านขวา */}
             <div className="md:w-1/2 w-full p-4">
               <img
-                src="../public/img/ครัว.jpg"
+                src="/ครัว.jpg"
                 alt="ครัว รังสรรค์"
                 className="w-full h-auto rounded-lg shadow-lg"
               />
